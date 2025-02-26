ae52f1b6be8f2c2c46431497fb04e74ba5f74d11
Keegan McGonigle <Keegan.McGonigle@prepass.com>
2025-02-26 12:24:56 -0700

    Fixed styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
 import { TopBar } from "@/components";
-import { Link, Outlet } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { cn } from "./lib/utils";
 
 function App() {
   return (
     <>
+      <TopBar className="fixed z-10" />
       <main
         className={cn(
           "w-full",
-          "relative flex min-h-svh flex-1 flex-col",
-          "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow"
+          "relative flex min-h-svh flex-1 flex-col mt-16"
         )}
       >
-        <TopBar />
-        <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{" "}
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-        </div>
-        <hr />
         <Outlet />
         <TanStackRouterDevtools />
       </main>
--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -2,28 +2,43 @@ import { cn } from "@/lib/utils";
 import { observer } from "mobx-react-lite";
 import { Separator } from "@/components/ui";
 import { ModeToggle } from "./mode-toggle";
+import { Link } from "@tanstack/react-router";
 
-const topNavStyle = [
-  "sticky",
-  "top-0",
-  "z-10",
-  "h-full",
-  "w-full",
-  "flex",
-  "items-center",
-  "gap-2",
-  "px-4",
-  "backdrop-blur-md",
-  "border-b-1",
-  "border-b-grey-7",
-];
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface TopBarProps extends React.ComponentPropsWithRef<"header"> {}
 
-export const TopBar = observer(() => {
+export const TopBar = observer(({ className, ...rest }: TopBarProps) => {
   return (
-    <header className={cn("absolute w-full h-16")}>
-      <div className={cn(topNavStyle)}>
+    <header className={cn("w-full h-16", className)} {...rest}>
+      <div
+        className={cn(
+          "sticky",
+          "top-0",
+          "z-10",
+          "h-full",
+          "w-full",
+          "flex",
+          "items-center",
+          "gap-2",
+          "px-4",
+          "backdrop-blur-md",
+          "border-b-1",
+          "border-b-grey-7"
+        )}
+      >
         <Separator orientation="vertical" className="mr-2 h-4" />
-        <ModeToggle />
+        <div className="flex justify-between w-full items-center">
+          <div className="flex gap-2">
+            <Link to="/" className="[&.active]:font-bold">
+              Home
+            </Link>{" "}
+            <Link to="/about" className="[&.active]:font-bold">
+              About
+            </Link>
+          </div>
+
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
